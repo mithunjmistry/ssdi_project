da2fda191e73a2963e0d0f8609e433c6e9290c9e
gaurav <gpahuja@uncc.edu>
2017-04-09 19:32:35 -0800

    Patient Discharge

--- ssdi_project/views.py
+++ ssdi_project/views.py
@@ -11,6 +11,7 @@ from datetime import datetime, timedelta
 from collections import OrderedDict, deque
 import calendar
 
+
 def get_boolean(value):
     if value.lower() == "yes":
         return True
@@ -19,9 +20,11 @@ def get_boolean(value):
     else:
         raise Exception
 
+
 def FactoryUserStatus(loggeduser):
     t = TypeOfUser.objects(username=loggeduser).first()
     return str(t.user_status)
+
 
 def signup_page(request):
     if request.POST:
@@ -43,8 +46,10 @@ def signup_page(request):
                 user = User.objects.create_user(username=username, email=uemail, password=upass)
                 user.save()
                 insured = get_boolean(insurance)
-                td = Patient.objects.create(username=username, email=uemail, first_name=first_name, last_name=last_name, gender=gender, dob=dob,
-                                            insured=insured, phone_number=phone, address=address, zipcode=zipcode, state=state)
+                td = Patient.objects.create(username=username, email=uemail, first_name=first_name, last_name=last_name,
+                                            gender=gender, dob=dob,
+                                            insured=insured, phone_number=phone, address=address, zipcode=zipcode,
+                                            state=state)
                 td.save()
                 tr = TypeOfUser.objects.create(username=username, user_status="patient")
                 tr.save()
@@ -54,6 +59,7 @@ def signup_page(request):
         else:
             return render(request, "register.html", {'error': "You didn't confirmed your password correctly!"})
     return render(request, "register.html", {'error': None})
+
 
 def login_page(request):
     if request.user.get_username():
@@ -70,14 +76,18 @@ def login_page(request):
             return render(request, "login.html", {'error': "Email or password incorrect!"})
     return render(request, "login.html", {'error': None})
 
+
 @login_required
 @never_cache
 def login_successful(request, loggeduserstatus, loggedusername):
-    return render(request, "{}.html".format(loggeduserstatus), {"message": "How you doing today: {}".format(loggedusername)})
+    return render(request, "{}.html".format(loggeduserstatus),
+                  {"message": "How you doing today: {}".format(loggedusername)})
+
 
 def logout_user(request):
     logout(request)
     return redirect(login_page)
+
 
 @login_required
 @never_cache
@@ -92,24 +102,169 @@ def check_beds(request, username):
     else:
         return redirect(login_page)
 
+
+class Bedsh():
+    room_type = StringField(required=True)
+    location = StringField(required=True, default="NC")
+    room_number = IntField(required=True)
+    bed_number = IntField(required=True)
+    patient_name = StringField(default=None)
+
+
+@login_required
+@never_cache
+def admit_patient(request, username):
+
+    if FactoryUserStatus(username) == "receptionist":
+        cnt = 0
+        t = Receptionist.objects(username=username).only('state').first()
+        if request.POST.get("patientID")!=None:
+            patientID = str(request.POST.get("patientID")).strip()
+            doctorName = str(request.POST.get("doctorName"))
+            if (Patient.objects(username=patientID).first()) is not None \
+                    and (Doctor.objects(username=doctorName).first()) is not None:
+                room_no = int(request.POST.get("room"))
+                room_type = str(request.POST.get("roomtype")).strip()
+                bed = int(request.POST.get("bed"))
+                Beds.objects(room_type=room_type,room_number=room_no,bed_number=bed,location=t.state).\
+                    update_one(set__patient_name=patientID)
+                Patient.objects(username=patientID).update(set__currently_admitted=True)
+                Patient.objects(username=patientID).update(set__doctor_name=doctorName.strip())
+                Doctor.objects(username=doctorName).update(push__patients_admitted=patientID)
+                cnt = 1
+                # enter into the db
+            else:
+                b = Beds.objects(location=t.state).exclude('location')
+                d = []
+                for i in b:
+                    if i.patient_name == None:
+                        bed = Bedsh()
+                        bed = i
+                        d.append(bed)
+                if patientID == None:
+                    return render(request,"admitpatient.html", {'error': "PatientID incorrect!",'d':d})
+                else:
+                    return render(request, "admitpatient.html", {'error': "Doctor incorrect!", 'd': d})
+        b = Beds.objects(location=t.state).exclude('location')
+        d = []
+        for i in b:
+            if i.patient_name == None:
+                bed = Bedsh()
+                bed = i
+                d.append(bed)
+        if cnt ==1:
+            return render(request, "admitpatient.html", {'d': d,'message':'Patient admitted'})
+        else:
+            return render(request, "admitpatient.html", {'d': d})
+    else:
+        return redirect(login_page)
+
+
 @login_required
 @never_cache
 def delete_user(request, username):
     try:
-        u = User.objects.get(username = username)
+        u = User.objects.get(username=username)
         u.delete()
         logout(request)
         return render(request, "user_deleted.html")
     except:
         return HttpResponse("Oops! Something went wrong!")
 
+def get_clean_timings_array(text):
+    to_return = []
+    if text.lower() != "holiday":
+        a = text.split(",")
+        b = a[0].split("to")
+        inter = []
+        t2_one = ""
+        t2_two = ""
+        if len(b) >= 2:
+            t1_one = b[0].strip()
+            t1_two = b[1].strip()
+        if len(a) >= 2:
+            c = a[1].strip()
+            d = c.split("to")
+            if len(d) >= 2:
+                t2_one = d[0].strip()
+                t2_two = d[1].strip()
+        inter.append(t1_one)
+        inter.append(t1_two)
+        for i in range(0, len(a)):
+            if len(inter[0]) < 6:
+                m = inter[0].split()
+                m = str(m[0]) + ":00 " + m[1]
+            else:
+                m = inter[0].strip()
+
+            if len(inter[1].strip()) < 6:
+                m1 = inter[1].split()
+                m1 = str(m1[0]) + ":00 " + m1[1]
+            else:
+                m1 = inter[1].strip()
+
+            to_return.append(m)
+            to_return.append(m1)
+        return to_return
+    else:
+        return ["Not available"]
+
+
 @login_required
 @never_cache
-def set_office_hours(request, username):
+def set_office_hours(request, username, day):
+    time_o = []
+    time = ""
+    message = None
     if FactoryUserStatus(username) == "doctor":
-        return "Hi"
-    else:
-        return redirect(login_page)
+        if request.POST:
+            time1 = request.POST.get("starttime")
+            time2 = request.POST.get("endtime")
+            time3 = request.POST.get("starttime2")
+            time4 = request.POST.get("endtime2")
+            new_time = "{} to {}, {} to {}".format(time1, time2, time3, time4)
+            Doctor.objects.filter(username=username, office_hours__day=day).update(
+                set__office_hours__S__time=new_time)
+            message = "Office hours updated successfully."
+            d = Doctor.objects(username=username).only("office_hours").first()
+            for i in d.office_hours:
+                time_o.append(i.time)
+            if day.lower() == "monday":
+                time = time_o[0]
+            elif day.lower() == "tuesday":
+                time = time_o[1]
+            elif day.lower() == "wednesday":
+                time = time_o[2]
+            elif day.lower() == "thursday":
+                time = time_o[3]
+            elif day.lower() == "friday":
+                time = time_o[4]
+            elif day.lower() == "saturday":
+                time = time_o[5]
+            else:
+                time = "Incorrect Day"
+            return render(request, "setofficehours.html", {"time": time, "message": message})
+        d = Doctor.objects(username=username).only("office_hours").first()
+        for i in d.office_hours:
+            time_o.append(i.time)
+        if day.lower() == "monday":
+            time = time_o[0]
+        elif day.lower() == "tuesday":
+            time = time_o[1]
+        elif day.lower() == "wednesday":
+            time = time_o[2]
+        elif day.lower() == "thursday":
+            time = time_o[3]
+        elif day.lower() == "friday":
+            time = time_o[4]
+        elif day.lower() == "saturday":
+            time = time_o[5]
+        else:
+            time = "Incorrect Day"
+        return render(request, "setofficehours.html", {"time": time, "message": message})
+
+
+
 
 @login_required
 @never_cache
@@ -134,13 +289,17 @@ def doctor_add(request, username):
                 try:
                     user = User.objects.create_user(username=username, email=uemail, password=upass)
                     user.save()
-                    td = Doctor.objects.create(username=username, email=uemail, first_name=first_name, last_name=last_name, gender=gender, dob=dob,
-                                                phone_number=phone, address=address, zipcode=zipcode, state=state, speciality=speciality, status=status)
+                    td = Doctor.objects.create(username=username, email=uemail, first_name=first_name,
+                                               last_name=last_name, gender=gender, dob=dob,
+                                               phone_number=phone, address=address, zipcode=zipcode, state=state,
+                                               speciality=speciality, status=status)
                     td.save()
                     tr = TypeOfUser.objects.create(username=username, user_status="doctor")
                     tr.save()
-                    message = "Hello Doctor,\nYou are registered in our group of hospitals successfully. Your username is {} and password is {}. Please change your password after logging in.\nHave a great day.".format(username, upass)
-                    send_mail(subject=("Registration Status"),message=str(message),from_email="ssdigroupproject@gmail.com", recipient_list=[uemail])
+                    message = "Hello Doctor,\nYou are registered in our group of hospitals successfully. Your username is {} and password is {}. Please change your password after logging in.\nHave a great day.".format(
+                        username, upass)
+                    send_mail(subject=("Registration Status"), message=str(message),
+                              from_email="ssdigroupproject@gmail.com", recipient_list=[uemail])
                     return render(request, "user_created.html")
                 except:
                     return render(request, "doctoradd.html", {'error': "This username is already being taken!"})
@@ -149,6 +308,7 @@ def doctor_add(request, username):
         return render(request, "doctoradd.html", {"error": None})
     else:
         return redirect(login_page)
+
 
 def show_doctors(request):
     full_name = []
@@ -165,6 +325,7 @@ def show_doctors(request):
     content = zip(full_name, speciality, state, username)
     return render(request, "ShowDoctors.html",
                   {"content": content})
+
 
 def option_maker(text):
     to_return = []
@@ -211,6 +372,7 @@ def option_maker(text):
     else:
         return ["Not available"]
 
+
 @login_required
 @never_cache
 def view_appointments_doctors(request, username):
@@ -227,6 +389,7 @@ def view_appointments_doctors(request, username):
         day.append(datetime.strptime(p.date, "%Y-%m-%d").strftime("%A"))
     content = zip(name, date, time, day)
     return render(request, "doctorvappt.html", {"content": content})
+
 
 @login_required
 @never_cache
@@ -247,6 +410,7 @@ def view_appointments_patients(request, username):
     content = zip(name, date, time, day, state)
     return render(request, "patientvappt.html", {"content": content})
 
+
 @never_cache
 def book_appointment(request, username):
     user_name = request.session["username"]
@@ -256,12 +420,14 @@ def book_appointment(request, username):
     doctor_name = request.session["name"]
     if request.user.get_username():
         patient = Patient.objects(username=request.user.get_username()).first()
-        patient.patient_appointments.append(PatientAppointments(date=date, time=time, state=state, under_doctor=doctor_name,
-                                                                doctor_username=username))
+        patient.patient_appointments.append(
+            PatientAppointments(date=date, time=time, state=state, under_doctor=doctor_name,
+                                doctor_username=username))
         patient.save()
         doctor = Doctor.objects(username=str(user_name)).first()
-        doctor.doctor_appointments.append(DoctorAppointments(date=date, time=time, patient="{} {}".format(patient.first_name, patient.last_name),
-                                                             patient_username=str(request.user.get_username()).strip()))
+        doctor.doctor_appointments.append(
+            DoctorAppointments(date=date, time=time, patient="{} {}".format(patient.first_name, patient.last_name),
+                               patient_username=str(request.user.get_username()).strip()))
         doctor.save()
         return render(request, "appointment_booked.html")
     else:
@@ -276,6 +442,7 @@ def book_appointment(request, username):
                 return render(request, "login.html", {'error': "Email or password incorrect!"})
         return render(request, "login.html", {'error': None})
 
+
 def view_time(request, username):
     error = None
     day = []
@@ -287,7 +454,7 @@ def view_time(request, username):
         for idx, i in enumerate(doctor.office_hours):
             day.append(i.day)
             time.append(i.time)
-            d[str(idx+1) + ". " + i.day] = option_maker(str(i.time))
+            d[str(idx + 1) + ". " + i.day] = option_maker(str(i.time))
 
         full_name = str(doctor.first_name) + " " + str(doctor.last_name)
         speciality = doctor.speciality
@@ -334,7 +501,9 @@ def view_time(request, username):
         else:
             error = "You cannot book appointment on Sunday"
     return render(request, "view_timings.html", {"content": content, "name": full_name, "speciality": speciality,
-                                  "consulting_fees": consulting_fees, "location": state, "contact": contact, "error": error})
+                                                 "consulting_fees": consulting_fees, "location": state,
+                                                 "contact": contact, "error": error})
+
 
 def test_database(request):
     '''
@@ -389,7 +558,7 @@ def test_database(request):
 
     tr = TypeOfUser.objects.create(username="pheebs", user_status="doctor")
     tr.save()
-    
+
     full_name = []
     speciality = []
     state = []
@@ -406,7 +575,14 @@ def test_database(request):
 
 
 def backend_adder(request):
+    Beds.objects(room_type='General', room_number=1, bed_number=1, location='NC'). \
+        update_one(set__patient_name=None)
+    Patient.objects(username='').update_one(set__currently_admitted=False)
+    Patient.objects(username='').update_one(set__doctor_name=None)
+    Doctor.objects(username='').update_one(pull__patients_admitted='')
     return HttpResponse("Added by backend.")
 
+
 def test_page(request):
-    return render(request, "appointment_booked.html")
+    Doctor.objects.filter(username="dcole0", office_hours__day="Monday").update(set__office_hours__S__time="9 am to 10 am, 5 pm to 6 pm")
+    return HttpResponse("hi")
